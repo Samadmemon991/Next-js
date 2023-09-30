9a3729aaca96ab39c64bcd5aa368b67d684dbcb5
samadmemon991 <samadmemon991@gmail.com>
2023-10-01 00:31:28 +0500

    company listing made dynamic

--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import Link from 'next/link';
 
 interface companiesInterface {
@@ -6,20 +7,12 @@ interface companiesInterface {
     logoSrc: string;
 }
 
-export default function companies() {
-    const companiesData: companiesInterface[] = [
-        {
-            id: 1,
-            name: 'Company-A',
-            logoSrc: 'https://i.pinimg.com/originals/ce/d6/6e/ced66ecfc53814d71f8774789b55cc76.png',
-        },
-        {
-            id: 2,
-            name: 'Company-B',
-            logoSrc: 'https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456',
-        },
+export default async function companies() {
 
-    ];
+    const prisma = new PrismaClient();
+
+
+    const companiesData: companiesInterface[] = await prisma.company.findMany();
 
     return (
         <div className="container mx-auto p-4">
@@ -31,7 +24,7 @@ export default function companies() {
                             <img
                                 src={companyData.logoSrc}
                                 alt={`${companyData.name} Logo`}
-                                className="w-16 h-16 mx-auto mb-2 rounded-lg shadow-lg group-hover:shadow-gray-500/50"
+                                className="h-16 mx-auto mb-2 rounded-lg shadow-lg group-hover:shadow-gray-500/50"
                             />
                             <h3 className="text-lg font-semibold text-center">{companyData.name}</h3>
                         </div>
