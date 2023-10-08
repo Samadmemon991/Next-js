8ebfa1cb38f5c17707a96a0dddcecb768665c6e6
samadmemon991 <samadmemon991@gmail.com>
2023-10-08 15:00:52 +0500

    Added error component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center mt-60">
       <div className="text-center">
         <FontAwesomeIcon icon={faSpinner} spinPulse className="text-5xl" />
         <p className="text-gray-600 mt-4">Loading...</p>
